f80a8e8f0edf7131c0c65d7a6810d87cd9c3dc23
Tony Aziz <tony@advizz.com>
2018-04-30 14:28:46 -0700

    update tests

--- contracts/EvoDistribution.sol
+++ contracts/EvoDistribution.sol
@@ -6,7 +6,7 @@ import './SafeMath.sol';
 import './Ownable.sol';
 
 /**
- * @title EVO token initial distribution
+ * @title Evo token initial distribution
  *
  * @dev Distribute purchasers, airdrop, reserve, and founder tokens
  */
@@ -16,17 +16,17 @@ contract EvoDistribution is Ownable {
   EvoToken public EVO;
 
   uint256 private constant decimalFactor = 10**uint256(18);
-  enum AllocationType { AIRDROP }
-  uint256 public constant INITIAL_SUPPLY   = 6000000000 * decimalFactor;
-  uint256 public AVAILABLE_TOTAL_SUPPLY    = 6000000000 * decimalFactor;
-  //uint256 public AVAILABLE_PRESALE_SUPPLY  =  230000000 * decimalFactor; // 100% Released at Token Distribution (TD)
-  //uint256 public AVAILABLE_FOUNDER_SUPPLY  =  150000000 * decimalFactor; // 33% Released at TD +1 year -> 100% at TD +3 years
+  enum AllocationType { PRESALE, FOUNDER, AIRDROP, ADVISOR, RESERVE, BONUS1, BONUS2, BONUS3 }
+  uint256 public constant INITIAL_SUPPLY   = 1000000000 * decimalFactor;
+  uint256 public AVAILABLE_TOTAL_SUPPLY    = 1000000000 * decimalFactor;
+  uint256 public AVAILABLE_PRESALE_SUPPLY  =  230000000 * decimalFactor; // 100% Released at Token Distribution (TD)
+  uint256 public AVAILABLE_FOUNDER_SUPPLY  =  150000000 * decimalFactor; // 33% Released at TD +1 year -> 100% at TD +3 years
   uint256 public AVAILABLE_AIRDROP_SUPPLY  =   10000000 * decimalFactor; // 100% Released at TD
-  //uint256 public AVAILABLE_ADVISOR_SUPPLY  =   20000000 * decimalFactor; // 100% Released at TD +7 months
-  //uint256 public AVAILABLE_RESERVE_SUPPLY  =  513116658 * decimalFactor; // 6.8% Released at TD +100 days -> 100% at TD +4 years
-  //uint256 public AVAILABLE_BONUS1_SUPPLY  =    39053330 * decimalFactor; // 100% Released at TD +1 year
-  //uint256 public AVAILABLE_BONUS2_SUPPLY  =     9354408 * decimalFactor; // 100% Released at TD +2 years
-  //uint256 public AVAILABLE_BONUS3_SUPPLY  =    28475604 * decimalFactor; // 100% Released at TD +3 years
+  uint256 public AVAILABLE_ADVISOR_SUPPLY  =   20000000 * decimalFactor; // 100% Released at TD +7 months
+  uint256 public AVAILABLE_RESERVE_SUPPLY  =  513116658 * decimalFactor; // 6.8% Released at TD +100 days -> 100% at TD +4 years
+  uint256 public AVAILABLE_BONUS1_SUPPLY  =    39053330 * decimalFactor; // 100% Released at TD +1 year
+  uint256 public AVAILABLE_BONUS2_SUPPLY  =     9354408 * decimalFactor; // 100% Released at TD +2 years
+  uint256 public AVAILABLE_BONUS3_SUPPLY  =    28475604 * decimalFactor; // 100% Released at TD +3 years
 
   uint256 public grandTotalClaimed = 0;
   uint256 public startTime;
@@ -34,8 +34,8 @@ contract EvoDistribution is Ownable {
   // Allocation with vesting information
   struct Allocation {
     uint8 AllocationSupply; // Type of allocation
-    //uint256 endCliff;       // Tokens are locked until
-    //uint256 endVesting;     // This is when the tokens are fully unvested
+    uint256 endCliff;       // Tokens are locked until
+    uint256 endVesting;     // This is when the tokens are fully unvested
     uint256 totalAllocated; // Total tokens allocated
     uint256 amountClaimed;  // Total tokens claimed
   }
@@ -44,7 +44,7 @@ contract EvoDistribution is Ownable {
   // List of admins
   mapping (address => bool) public airdropAdmins;
 
-  // Keeps track of whether or not a Evo airdrop has been made to a particular address
+  // Keeps track of whether or not a 250 Evo airdrop has been made to a particular address
   mapping (address => bool) public airdrops;
 
   modifier onlyOwnerOrAdmin() {
@@ -56,12 +56,12 @@ contract EvoDistribution is Ownable {
   event LogEvoClaimed(address indexed _recipient, uint8 indexed _fromSupply, uint256 _amountClaimed, uint256 _totalAllocated, uint256 _grandTotalClaimed);
 
   /**
-    * @dev Constructor function - Set the evo token address
+    * @dev Constructor function - Set the Evo token address
     * @param _startTime The time when EvoDistribution goes live
     */
   function EvoDistribution(uint256 _startTime) public {
     require(_startTime >= now);
-    require(AVAILABLE_TOTAL_SUPPLY == AVAILABLE_AIRDROP_SUPPLY);
+    require(AVAILABLE_TOTAL_SUPPLY == AVAILABLE_PRESALE_SUPPLY.add(AVAILABLE_FOUNDER_SUPPLY).add(AVAILABLE_AIRDROP_SUPPLY).add(AVAILABLE_ADVISOR_SUPPLY).add(AVAILABLE_BONUS1_SUPPLY).add(AVAILABLE_BONUS2_SUPPLY).add(AVAILABLE_BONUS3_SUPPLY).add(AVAILABLE_RESERVE_SUPPLY));
     startTime = _startTime;
     EVO = new EvoToken(this);
   }
@@ -69,10 +69,10 @@ contract EvoDistribution is Ownable {
   /**
     * @dev Allow the owner of the contract to assign a new allocation
     * @param _recipient The recipient of the allocation
-    * @param _totalAllocated The total amount of EVO available to the receipient (after vesting)
-    * @param _supply The EVO supply the allocation will be taken from
+    * @param _totalAllocated The total amount of Evo available to the receipient (after vesting)
+    * @param _supply The Evo supply the allocation will be taken from
     */
-  /*function setAllocation (address _recipient, uint256 _totalAllocated, AllocationType _supply) onlyOwner public {
+  function setAllocation (address _recipient, uint256 _totalAllocated, AllocationType _supply) onlyOwner public {
     require(allocations[_recipient].totalAllocated == 0 && _totalAllocated > 0);
     require(_supply >= AllocationType.PRESALE && _supply <= AllocationType.BONUS3);
     require(_recipient != address(0));
@@ -100,7 +100,7 @@ contract EvoDistribution is Ownable {
     }
     AVAILABLE_TOTAL_SUPPLY = AVAILABLE_TOTAL_SUPPLY.sub(_totalAllocated);
     LogNewAllocation(_recipient, _supply, _totalAllocated, grandTotalAllocated());
-  }*/
+  }
 
   /**
     * @dev Add an airdrop admin
@@ -120,8 +120,8 @@ contract EvoDistribution is Ownable {
     {
         if (!airdrops[_recipient[i]]) {
           airdrops[_recipient[i]] = true;
-          require(EVO.transfer(_recipient[i], 1000 * decimalFactor));
-          airdropped = airdropped.add(1000 * decimalFactor);
+          require(EVO.transfer(_recipient[i], 250 * decimalFactor));
+          airdropped = airdropped.add(250 * decimalFactor);
         }
     }
     AVAILABLE_AIRDROP_SUPPLY = AVAILABLE_AIRDROP_SUPPLY.sub(airdropped);
@@ -135,16 +135,16 @@ contract EvoDistribution is Ownable {
     */
   function transferTokens (address _recipient) public {
     require(allocations[_recipient].amountClaimed < allocations[_recipient].totalAllocated);
-    //require(now >= allocations[_recipient].endCliff);
+    require(now >= allocations[_recipient].endCliff);
     require(now >= startTime);
     uint256 newAmountClaimed;
-    /*if (allocations[_recipient].endVesting > now) {
+    if (allocations[_recipient].endVesting > now) {
       // Transfer available amount based on vesting schedule and allocation
       newAmountClaimed = allocations[_recipient].totalAllocated.mul(now.sub(startTime)).div(allocations[_recipient].endVesting.sub(startTime));
     } else {
       // Transfer total allocated (minus previously claimed tokens)
       newAmountClaimed = allocations[_recipient].totalAllocated;
-    }*/
+    }
     uint256 tokensToTransfer = newAmountClaimed.sub(allocations[_recipient].amountClaimed);
     allocations[_recipient].amountClaimed = newAmountClaimed;
     require(EVO.transfer(_recipient, tokensToTransfer));
@@ -152,7 +152,7 @@ contract EvoDistribution is Ownable {
     LogEvoClaimed(_recipient, allocations[_recipient].AllocationSupply, tokensToTransfer, newAmountClaimed, grandTotalClaimed);
   }
 
-  // Returns the amount of EVO allocated
+  // Returns the amount of Evo allocated
   function grandTotalAllocated() public view returns (uint256) {
     return INITIAL_SUPPLY - AVAILABLE_TOTAL_SUPPLY;
   }
